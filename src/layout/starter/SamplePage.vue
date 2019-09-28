0c07ea9ca88a7e225ccc2375f7139bd86c48bc4d
juan-carvajal <39658930+juan-carvajal@users.noreply.github.com>
2019-09-28 17:52:19 -0500

    Updated with own components

--- src/layout/starter/SamplePage.vue
+++ src/layout/starter/SamplePage.vue
@@ -1,5 +1,138 @@
 <template>
-  <div class="row"><h3>Starter page</h3></div>
+  <div class="row">
+            <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <div class="text-center">
+            <h1 class="display-2" style="color:rgb(78, 115, 223);">VIP PROJECT</h1>
+            <h1 class="display-4" style="color:rgb(78, 115, 223);">ICESI University, Cali, Colombia</h1>
+          </div>
+
+
+
+          <!-- Content Row -->
+
+          <div class="row">
+            <div class="col">
+              <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">About VIP - the Vertically Integrated Projects Program
+                  </h6>
+                </div>
+                <div class="card-body">
+                  <p>The Vertically Integrated Projects (VIP) Program unites undergraduate education and faculty
+                    research in a team-based context.
+                    Undergraduate VIP students earn academic credits, while faculty and graduate students benefit from
+                    the design/discovery efforts of their teams.<br>
+                    This program allows to:
+                    <ul>
+                      <li>Create long-term research & development experiences.</li>
+                      <li>Cultivates leadership & mentoring.</li>
+                      <li>Benefits faculty research programs.</li>
+                    </ul>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- Content Row -->
+          <div class="row">
+
+            <!-- Description (Welcome)-->
+            <div class="col">
+              <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">About ALIEN</h6>
+                </div>
+                <!-- Card Body -->
+                <div class="card-body">
+                  <div class="text-center">
+                   <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 60%;"
+                      src="https://github.com/icesivip/alienbackend/blob/test/src/main/resources/static/img/Vipcierre.jpg?raw=true" alt="">
+                  </div>
+                  <p>Algorithms Lab for Industrial Engineering Needs, a team from another world, is a project under
+                    construction as part of the framework of Icesi VIP from
+                    <a target="_blank" rel="nofollow" href="https://www.icesi.edu.co/">University Icesi</a>, Cali
+                    Colombia, associated with <a target="_blank" rel="nofollow" href="http://www.vip.gatech.edu/">VIP
+                      Gatech Program.</a></p>
+                </div>
+              </div>
+            </div>
+
+
+
+          <!-- Content Row -->
+          <div class="row">
+
+            <!-- Content Column -->
+            <div class="col">
+
+              <!-- Goals -->
+              <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Goals</h6>
+                </div>
+                <div class="card-body">
+                  <div class="text-center">
+                    <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;"
+                      src="https://cdn.pixabay.com/photo/2017/01/05/14/13/target-1955257_960_720.jpg" alt="">
+                  </div>
+                  <p>
+                    We aim to:
+                    <ul>
+                      <li>To develop a educational and integrated framework software that implement methods for decision
+                        making on bussiness.</li>
+                      <li>To work on bussiness intelligence tools of intensive academic use.</li>
+                      <li>To bring a completely practical laboratory to validate methods and algorithms used on
+                        industry.</li>
+                      <li>To support teaching strategies by developing and using software tools.</li>
+                    </ul>
+                  </p>
+                </div>
+              </div>
+            </div>
+
+
+            <div class="col">
+
+              <!-- Issues -->
+              <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Issues Involved or Addressed</h6>
+                </div>
+                <div class="card-body">
+                  <p>
+                    Algorithms and methods widely used for quantitative systems for business involving operations
+                    research and
+                    management science, all of them used mainly in higher education.
+                  </p>
+                </div>
+              </div>
+
+              <!-- Methods and Technologies -->
+              <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Methods and Technologies</h6>
+                </div>
+                <div class="card-body">
+                  <p>Rapid prototype development, Iterative and incremental development, Open source development technologies, Bussiness inteligence.</p>
+                </div>
+              </div>
+
+              <!-- Modules -->
+
+
+
+            </div>
+          </div>
+
+        </div>
+        <!-- /.container-fluid -->
+
+      </div>
+
+  </div>
+
 </template>
 <script>
 export default {
--- src/main.js
+++ src/main.js
@@ -15,8 +15,8 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import RouterPrefetch from 'vue-router-prefetch'
 import App from "./App";
-// TIP: change to import router from "./router/starterRouter"; to start with a clean layout
-import router from "./router/index";
+// TIP: change to import router from "./router/starterRouter"; to start with a clean layout   Full:"./router/index"
+import router from "./router/starterRouter";
 
 import BlackDashboard from "./plugins/blackDashboard";
 import i18n from "./i18n"
